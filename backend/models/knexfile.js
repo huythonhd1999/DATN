2789d1bb8bfba7d67f4e4edf46622d0e0f3a1a76
henry.vu <henry.vu@avepoint.com>
2023-07-18 18:12:20 +0700

    update

--- backend/models/knexfile.js
+++ backend/models/knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host : '52.139.214.15',
+      host : '127.0.0.1',
       port : 3306,
-      user : 'DAHTTT',
-      password : 'haiduong_1999',
+      user : 'root',
+      password : '21081999',
       database : 'DATN'
   }
 }};
